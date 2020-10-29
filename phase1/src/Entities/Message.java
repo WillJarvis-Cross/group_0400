fd566d1d731ef635e09b7b9dcfbef50768bf1a6f
Will Jarvis-Cross <will.jarviscross@mail.utoronto.ca>
2020-10-29 13:43:29 -0400

    added message class

---
+++ phase1/src/Entities/Message.java
@@ -0,0 +1,32 @@
+import java.lang.String;
+
+public class Message {
+    // This message's content
+    private final String content;
+
+    // The user who sent the message
+    private final String senderUsername;
+
+    // The user receiving the message
+    private final String receiverUsername;
+
+    public Message(String content, String senderUsername, String receiverUsername){
+        this.content = content;
+        this.senderUsername = senderUsername;
+        this.receiverUsername = receiverUsername;
+    }
+
+    // Return the content of the message
+    public String getContent(){
+        return content;
+    }
+    //return the username of the User who sent the message
+    public String getSender(){
+        return senderUsername;
+    }
+    // Return the username of the User who received the message
+    public String getReceiver(){
+        return receiverUsername;
+    }
+}
+
